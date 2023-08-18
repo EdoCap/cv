0c2cad7a3ea5aca180908f6b0373b36dca68c6e7
Edoardo Cappello <edoardo.cappello@gmail.com>
2023-08-18 17:06:41 +0100

    Fix indentation spaces

--- Tests/edoardo-webpageTests/edoardo_webpageTests.swift
+++ Tests/edoardo-webpageTests/edoardo_webpageTests.swift
@@ -2,10 +2,10 @@ import XCTest
 @testable import edoardo_webpage
 
 final class edoardo_webpageTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(edoardo_webpage().text, "Hello, World!")
-    }
+  func testExample() throws {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct
+    // results.
+    XCTAssertEqual(edoardo_webpage().text, "Hello, World!")
+  }
 }
