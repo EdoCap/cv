fab4378ba0326ae4007d0dc494280aaa733ce736
Edoardo Cappello <edoardo.cappello@gmail.com>
2023-08-18 16:58:56 +0100

    Add swift package scaffold

---
+++ README.md
@@ -0,0 +1,3 @@
+# edoardo-webpage
+
+A description of this package.
---
+++ Sources/edoardo-webpage/edoardo_webpage.swift
@@ -0,0 +1,8 @@
+@main
+public struct edoardo_webpage {
+    public private(set) var text = "Hello, World!"
+
+    public static func main() {
+        print(edoardo_webpage().text)
+    }
+}
